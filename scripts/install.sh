5c47ca0b9d2cf6bae5b865cb054747a44e9629c5
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-10-18 15:37:07 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,7 +8,8 @@ if [[ "$reply" == "y" ]]; then
     rm /tmp/stk.tar.xz
     curl -L https://github.com/supertuxkart/stk-code/releases/download/1.4/SuperTuxKart-1.4-linux-x86_64.tar.xz -o /tmp/stk.tar.xz
     echo "Done. Configuring shorcuts..."
-    curl -L https://raw.githubusercontent.com/techarecrazy/STK-Installer/main/SuperTuxKart.desktop -o $HOME/.local/share/applications/SuperTuxKart.desktop
+    mkdir ~/.local/share/applications
+    curl -L https://raw.githubusercontent.com/techarecrazy/STK-Installer/main/SuperTuxKart.desktop -o ~/.local/share/applications/SuperTuxKart.desktop
     echo "Done. Extracting The Game..."
     sudo rm -rf /usr/local/games/SuperTuxKart
     sudo tar -xf /tmp/stk.tar.xz -C /usr/local/games
