727d3df4a2c9adf44829361320b973ecd015ba11
techarecrazy <118292773+techarecrazy@users.noreply.github.com>
2023-12-16 08:08:04 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,13 +7,14 @@ if [[ "$reply" == "y" ]]; then
     echo "Done. Downloading Super Tux Kart..."
     rm /tmp/stk.tar.xz
     curl -L https://github.com/supertuxkart/stk-code/releases/download/1.4/SuperTuxKart-1.4-linux-x86_64.tar.xz -o /tmp/stk.tar.xz
-    echo "Done. Configuring shorcuts..."
-    sudo rm -rf /usr/share/applications/SuperTuxKart.desktop
-    sudo curl -L https://raw.githubusercontent.com/techarecrazy/STK-Installer/main/SuperTuxKart.desktop -o /usr/share/applications/SuperTuxKart.desktop
     echo "Done. Extracting The Game..."
     sudo rm -rf /usr/games/SuperTuxKart
     sudo tar -xf /tmp/stk.tar.xz -C /usr/games
     sudo mv /usr/games/SuperTuxKart-1.4-linux-x86_64 /usr/games/SuperTuxKart
+    echo "Done. Configuring shorcuts..."
+    sudo rm -rf /usr/share/applications/SuperTuxKart.desktop
+    sudo cat /usr/games/SuperTuxKart/SuperTuxKart.desktop > /usr/share/applications/SuperTuxKart.desktop
+    sudo curl -L https://raw.githubusercontent.com/techarecrazy/STK-Installer/main/SuperTuxKart.desktop >> /usr/share/applications/SuperTuxKart.desktop
     echo "Done!"
 else
   exit 1
